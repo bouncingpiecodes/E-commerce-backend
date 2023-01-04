1970a933e70d2918ff351f24308833bee215244b
“kara <bouncingpie@icloud.com>
2023-01-04 00:26:03 -0500

    adding associations

--- models/index.js
+++ models/index.js
@@ -1,14 +1,24 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: "product_tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
 
